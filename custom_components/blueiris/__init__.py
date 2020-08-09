0ff674b90cb1ab9a727c24f2b41b29ddff71c9ac
David Ryan <djrconcepts@gmail.com>
2020-08-09 14:45:02 -0700

    Recover Hassio Crash plus major changes

--- custom_components/blueiris/__init__.py
+++ custom_components/blueiris/__init__.py
@@ -3,96 +3,64 @@ This component provides support for Blue Iris.
 For more details about this component, please refer to the documentation at
 https://home-assistant.io/components/blueiris/
 """
-import sys
 import logging
+import sys
 
-import voluptuous as vol
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
-from homeassistant.const import (
-    CONF_ROOM, CONF_EXCLUDE, CONF_NAME, CONF_HOST, CONF_PORT, CONF_PASSWORD,
-    CONF_USERNAME, CONF_SSL, CONF_ID)
-from homeassistant.helpers import config_validation as cv
-
-from .const import VERSION
-from .const import *
-from .blue_iris_data import BlueIrisData
-from .home_assistant import BlueIrisHomeAssistant
+from .helpers import async_set_ha, clear_ha, get_ha, handle_log_level
+from .helpers.const import *
 
 _LOGGER = logging.getLogger(__name__)
 
-CAMERA_SCHEMA = vol.Schema({
-    vol.Required(CONF_ID): cv.string,
-    vol.Optional(CONF_NAME, default=None): cv.string,
-    vol.Optional(CONF_ROOM, default=None): cv.string,
-})
 
-PROFILE_SCHEMA = vol.Schema({
-    vol.Required(CONF_PROFILE_ARMED): vol.All(vol.Coerce(int),
-                                              vol.Range(min=-1, max=7)),
-    vol.Required(CONF_PROFILE_UNARMED): vol.All(vol.Coerce(int),
-                                                vol.Range(min=-1, max=7)),
-})
-
-CONFIG_SCHEMA = vol.Schema({
-    DOMAIN: vol.Schema({
-        vol.Required(CONF_HOST): cv.string,
-        vol.Required(CONF_PORT): cv.string,
-        vol.Required(CONF_CAMERAS):
-            vol.All(cv.ensure_list, [vol.Any(CAMERA_SCHEMA)]),
-        vol.Optional(CONF_EXCLUDE):
-            vol.All(cv.ensure_list, [vol.In(SYSTEM_CAMERA_CONFIG)]),
-        vol.Optional(CONF_PROFILE): PROFILE_SCHEMA,
-        vol.Optional(CONF_USERNAME): cv.string,
-        vol.Optional(CONF_PASSWORD): cv.string,
-        vol.Optional(CONF_SSL, default=False): cv.boolean,
-    }),
-}, extra=vol.ALLOW_EXTRA)
+async def async_setup(hass, config):
+    return True
 
 
-def setup(hass, config):
-    """Set up a Blue Iris component."""
-    ha = None
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up a BlueIris component."""
     initialized = False
 
     try:
-        conf = config[DOMAIN]
-        scan_interval = SCAN_INTERVAL
-        host = conf.get(CONF_HOST)
-        port = conf.get(CONF_PORT)
-        cameras = conf.get(CONF_CAMERAS)
-        exclude = conf.get(CONF_EXCLUDE)
-        username = conf.get(CONF_USERNAME)
-        password = conf.get(CONF_PASSWORD)
-        ssl = conf.get(CONF_SSL)
-        profile = conf.get(CONF_PROFILE)
+        await handle_log_level(hass, entry)
 
-        bi_data = BlueIrisData(host, port, cameras, username, password, ssl,
-                               exclude, profile, scan_interval, cv.template)
+        _LOGGER.debug(f"Starting async_setup_entry of {DOMAIN}")
+        entry.add_update_listener(async_options_updated)
 
-        ha = BlueIrisHomeAssistant(hass, scan_interval, bi_data.cast_url)
+        await async_set_ha(hass, entry)
 
-        hass.data[DATA_BLUEIRIS] = bi_data
-
-        configuration_errors = bi_data.get_configuration_errors()
-
-        if configuration_errors is not None:
-            all_errors = "<br /> - ".join(configuration_errors)
-            error_message = f"<b>Errors while loading configuration:</b>" \
-                f"<br />{all_errors}"
-
-            ha.notify_error_message(error_message)
-        else:
-            camera_list = bi_data.get_all_cameras()
-
-            ha.initialize(bi_data.update, camera_list)
-
-            initialized = True
+        initialized = True
 
     except Exception as ex:
         exc_type, exc_obj, tb = sys.exc_info()
         line_number = tb.tb_lineno
 
-        if ha is not None:
-            ha.notify_error(ex, line_number)
+        _LOGGER.error(f"Failed to load BlueIris, error: {ex}, line: {line_number}")
 
     return initialized
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    ha = get_ha(hass, entry.entry_id)
+
+    if ha is not None:
+        await ha.async_remove()
+
+    clear_ha(hass, entry.entry_id)
+
+    return True
+
+
+async def async_options_updated(hass: HomeAssistant, entry: ConfigEntry):
+    """Triggered by config entry options updates."""
+    await handle_log_level(hass, entry)
+
+    _LOGGER.info(f"async_options_updated, Entry: {entry.as_dict()} ")
+
+    ha = get_ha(hass, entry.entry_id)
+
+    if ha is not None:
+        await ha.async_update_entry(entry)
--- custom_components/blueiris/binary_sensor.py
+++ custom_components/blueiris/binary_sensor.py
@@ -3,160 +3,27 @@ Support for Blue Iris binary sensors.
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/binary_sensor.blueiris/
 """
-import asyncio
 import logging
-from homeassistant.core import callback
-from homeassistant.components import mqtt
-from homeassistant.components.binary_sensor import (BinarySensorDevice)
-from homeassistant.const import (CONF_NAME, STATE_ON, STATE_OFF)
-from homeassistant.components.mqtt import (
-    MqttAvailability, CONF_PAYLOAD_AVAILABLE, DEFAULT_PAYLOAD_AVAILABLE,
-    CONF_QOS, CONF_PAYLOAD_NOT_AVAILABLE, DEFAULT_PAYLOAD_NOT_AVAILABLE,
-    DEFAULT_QOS)
 
-from .const import *
+from .binary_sensors import get_binary_sensor
+from .helpers.const import *
+from .models.base_entity import async_setup_base_entry
 
 _LOGGER = logging.getLogger(__name__)
 
-DEPENDENCIES = [DOMAIN, 'mqtt']
+DEPENDENCIES = [DOMAIN, "mqtt"]
+
+CURRENT_DOMAIN = DOMAIN_BINARY_SENSOR
 
 
-@asyncio.coroutine
-def async_setup_platform(hass, config, async_add_entities,
-                         discovery_info=None):
-    """Set up the Blue Iris binary sensor."""
-    bi_data = hass.data.get(DATA_BLUEIRIS)
-
-    if not bi_data:
-        return
-
-    cameras = bi_data.get_all_cameras()
-
-    bi_binary_sensor_list = []
-
-    force_update = DEFAULT_FORCE_UPDATE
-
-    mqtt_availability_config = {
-        CONF_PAYLOAD_AVAILABLE: DEFAULT_PAYLOAD_AVAILABLE,
-        CONF_PAYLOAD_NOT_AVAILABLE: DEFAULT_PAYLOAD_NOT_AVAILABLE,
-        CONF_QOS: DEFAULT_QOS
-    }
-
-    for camera_id in cameras:
-        camera = cameras[camera_id]
-        _LOGGER.debug(f"Processing new camera[{camera_id}]: {camera}")
-
-        if camera_id not in [
-                ATTR_SYSTEM_CAMERA_ALL_ID, ATTR_SYSTEM_CAMERA_CYCLE_ID
-        ]:
-            # Create camera motion, audio, external, and watchdog MQTT topics.
-            for t in ['MOTION', 'AUDIO', 'EXTERNAL', 'WATCHDOG']:
-                state = None
-
-                state_topic = f"BlueIris/{camera_id}/{t}"
-
-                if t == 'WATCHDOG':
-                    device_class = 'connectivity'
-
-                    # Invert sense of sensor message, as 'WATCHDOG'
-                    # triggers 'ON' during a disconnection.
-                    payload_on = DEFAULT_PAYLOAD_OFF
-                    payload_off = DEFAULT_PAYLOAD_ON
-                    # Assume we start in the 'connected' state.
-                    state = STATE_ON
-                else:
-                    if t == 'MOTION':
-                        # Hardcode topic to default trigger zone 'A'.
-                        # If more than one trigger zone is in use, additional
-                        # sensors will have to be created manually.
-                        state_topic = f"BlueIris/{camera_id}/MOTION_A"
-
-                        device_class = 'motion'
-                    if t == 'AUDIO':
-                        device_class = 'sound'
-                    if t == 'EXTERNAL':
-                        device_class = 'plug'
-
-                    payload_on = DEFAULT_PAYLOAD_ON
-                    payload_off = DEFAULT_PAYLOAD_OFF
-
-                binary_sensor_name = f"{camera[CONF_NAME]} {t.lower()}"
-
-                bi_motion_binary_sensor = BlueIrisBinarySensor(
-                    binary_sensor_name, state_topic, device_class,
-                    force_update, payload_on, payload_off,
-                    mqtt_availability_config, state)
-
-                bi_binary_sensor_list.append(bi_motion_binary_sensor)
-
-                _LOGGER.debug("Blue Iris Binary Sensor created:"
-                              f" {bi_motion_binary_sensor},"
-                              f" state_topic: {state_topic}")
-
-    # Add component entities asynchronously.
-    async_add_entities(bi_binary_sensor_list, True)
+async def async_setup_entry(hass, config_entry, async_add_devices):
+    """Set up the BlueIris Binary Sensor."""
+    await async_setup_base_entry(
+        hass, config_entry, async_add_devices, CURRENT_DOMAIN, get_binary_sensor
+    )
 
 
-class BlueIrisBinarySensor(MqttAvailability, BinarySensorDevice):
-    """Representation a binary sensor that is updated by MQTT."""
-    def __init__(self, name, state_topic, device_class, force_update,
-                 payload_on, payload_off, mqtt_availability_config,
-                 state=None):
-        """Initialize the MQTT binary sensor."""
-        super().__init__(mqtt_availability_config)
-        self._name = name
-        self._state = state
-        self._state_topic = state_topic
-        self._device_class = device_class
-        self._payload_on = payload_on
-        self._payload_off = payload_off
-        self._force_update = force_update
+async def async_unload_entry(hass, config_entry):
+    _LOGGER.info(f"async_unload_entry {CURRENT_DOMAIN}: {config_entry}")
 
-    @asyncio.coroutine
-    def async_added_to_hass(self):
-        """Subscribe MQTT events."""
-        yield from super().async_added_to_hass()
-
-        @callback
-        def state_message_received(message):
-            """Handle a new received MQTT state message."""
-            if message.payload == self._payload_on:
-                self._state = True
-            elif message.payload == self._payload_off:
-                self._state = False
-            else:
-                """Payload is not for this entity."""
-                _LOGGER.warning("No matching payload found for entity:"
-                                f" {self._name} with state_topic:"
-                                f" {self._state_topic}")
-                return
-
-            self.async_schedule_update_ha_state()
-
-        yield from mqtt.async_subscribe(self.hass, self._state_topic,
-                                        state_message_received, DEFAULT_QOS)
-
-    @property
-    def should_poll(self):
-        """Return the polling state."""
-        return False
-
-    @property
-    def name(self):
-        """Return the name of the binary sensor."""
-        return self._name
-
-    @property
-    def is_on(self):
-        """Return true if the binary sensor is on."""
-        return self._state
-
-    @property
-    def device_class(self):
-        """Return the class of this sensor."""
-        return self._device_class
-
-    @property
-    def force_update(self):
-        """Force update."""
-        return self._force_update
+    return True
